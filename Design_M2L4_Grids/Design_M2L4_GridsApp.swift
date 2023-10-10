59eef6270fd568c4ac75d275b6b4a694f3b9ba0e
Tom Montgomery <montgomery3681@gmail.com>
2023-10-09 21:12:56 -0600

    Initial Commit

---
+++ Design_M2L4_Grids/Design_M2L4_GridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M2L4_GridsApp.swift
+//  Design_M2L4_Grids
+//
+//  Created by tom montgomery on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M2L4_GridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
